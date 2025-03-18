e0adea9647bbf9d237a84f820c41a26ad3f2b197
Adrian Belarte <adrian.belarte.it@gmail.com>
2025-03-18 18:29:06 +0100

    Trabajo finalizado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 import peliculas from './peliculas.js'
 
+
+//separamos las peliculas por genero
+const accion = peliculas.filter(pelicula => pelicula.genre_ids.includes(28));
+const thriller = peliculas.filter(pelicula => pelicula.genre_ids.includes(53));
+const aventura = peliculas.filter(pelicula => pelicula.genre_ids.includes(12));
+
+function crearElementoPelicula(pelicula) {
+    const div = document.createElement('div');
+    div.classList.add('pelicula');
+    div.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}" />
+        <h3>${pelicula.title}</h3>
+    `;
+    return div;
+}
+
+const contenedorAccion = document.getElementById('genero-28');
+accion.forEach(pelicula => {
+    contenedorAccion.appendChild(crearElementoPelicula(pelicula));
+});
+
+const contenedorThriller = document.getElementById('genero-53');
+thriller.forEach(pelicula => {
+    contenedorThriller.appendChild(crearElementoPelicula(pelicula));
+    });
+
+const contenedorAventura = document.getElementById('genero-12');
+aventura.forEach(pelicula => {
+    contenedorAventura.appendChild(crearElementoPelicula(pelicula));
+    });
+
+mostrarPeliculasPorGenero();
